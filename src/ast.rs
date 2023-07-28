5718f9f88d2764b423b93fa2a7b6b89e47683190
alwayth <ksiwt.jp@gmail.com>
2023-07-28 23:38:35 +0900

    preparing the ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,10 @@
-use std::fmt;
+use std::{any::Any, fmt};
 
 use crate::token::Token;
 
 pub trait Node {
     fn token_literal(&self) -> String;
+    fn string(&self) -> String;
 }
 
 pub trait Statement: Node {
@@ -12,6 +13,7 @@ pub trait Statement: Node {
 
 pub trait Expression: Node {
     fn expression_node(&self);
+    fn as_any(&self) -> &dyn Any;
 }
 
 impl fmt::Debug for dyn Statement {
@@ -38,17 +40,47 @@ impl Program {
             return String::from("");
         }
     }
+
+    fn string(&mut self) -> String {
+        let mut out = String::new();
+        for stmt in self.statements.iter() {
+            out.push_str(&stmt.string());
+        }
+
+        out
+    }
 }
 
 pub struct LetStatement {
     pub token: Token, // the 'let' token.
     pub name: Identifier,
+    pub value: Box<dyn Expression>,
 }
+
+impl LetStatement {}
 
 impl Node for LetStatement {
     fn token_literal(&self) -> String {
         let l = self.token.literal.to_string();
         l
+    }
+
+    fn string(&self) -> String {
+        let mut out = String::new();
+        out.push_str(&self.token_literal());
+        out.push_str(" ");
+
+        out.push_str(&self.name.value);
+        out.push_str(" = ");
+
+        match self.value.as_any().downcast_ref::<Identifier>() {
+            Some(val) => out.push_str(&val.string()),
+            _ => (),
+        };
+
+        out.push_str(";");
+
+        out
     }
 }
 
@@ -61,6 +93,8 @@ pub struct ReturnStatement {
     pub return_value: Box<dyn Expression>,
 }
 
+impl ReturnStatement {}
+
 impl Statement for ReturnStatement {
     fn statement_node(&self) {}
 }
@@ -70,6 +104,54 @@ impl Node for ReturnStatement {
         let l = self.token.literal.to_string();
         l
     }
+
+    fn string(&self) -> String {
+        let mut out = String::new();
+        out.push_str(&self.token_literal());
+
+        match self.return_value.as_any().downcast_ref::<Identifier>() {
+            Some(idt) => out.push_str(&idt.string()),
+            _ => (),
+        };
+
+        out.push_str(" ");
+        out.push_str(";");
+
+        out
+    }
+}
+
+pub struct ExpressionStatement {
+    pub token: Token, // the first token of the expression
+    pub expression: Box<dyn Expression>,
+}
+
+impl ExpressionStatement {}
+
+impl Statement for ExpressionStatement {
+    fn statement_node(&self) {}
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> String {
+        let l = self.token.literal.to_string();
+        l
+    }
+
+    fn string(&self) -> String {
+        let out = String::new();
+
+        match self
+            .expression
+            .as_any()
+            .downcast_ref::<Box<dyn Expression>>()
+        {
+            Some(e) => return e.string(),
+            _ => (),
+        };
+
+        out
+    }
 }
 
 pub struct Identifier {
@@ -77,13 +159,58 @@ pub struct Identifier {
     pub value: String,
 }
 
+impl Identifier {}
+
 impl Node for Identifier {
     fn token_literal(&self) -> String {
         let l = self.token.literal.to_string();
+        l
+    }
+
+    fn string(&self) -> String {
+        let l = self.value.to_string();
         l
     }
 }
 
 impl Expression for Identifier {
     fn expression_node(&self) {}
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::token::{Token, TokenKind};
+
+    use crate::ast::{Expression, Identifier, LetStatement, Program};
+
+    #[test]
+    fn test_string() {
+        let mut program = Program {
+            statements: vec![Box::new(LetStatement {
+                token: Token {
+                    token_kind: TokenKind::Let,
+                    literal: String::from("let"),
+                },
+                name: Identifier {
+                    token: Token {
+                        token_kind: TokenKind::Ident,
+                        literal: String::from("myVar"),
+                    },
+                    value: String::from("myVar"),
+                },
+                value: Box::new(Identifier {
+                    token: Token {
+                        token_kind: TokenKind::Ident,
+                        literal: String::from("anotherVar"),
+                    },
+                    value: String::from("anotherVar"),
+                }) as Box<dyn Expression>,
+            })],
+        };
+
+        assert_eq!(program.string(), "let myVar = anotherVar;")
+    }
 }
